03b6999c99798e9263c6cc92088872306c71e03d
Harsh Shukla <108127262+harshshukla2807@users.noreply.github.com>
2026-07-20 04:02:08 +0530

    backend setup completed db working successfully

---
+++ lib/mongodb.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env.local");
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongooseConnection:
+    | {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+      }
+    | undefined;
+}
+
+const cached = global.mongooseConnection ?? {
+  conn: null,
+  promise: null,
+};
+
+global.mongooseConnection = cached;
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
